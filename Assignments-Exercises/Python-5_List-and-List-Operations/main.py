294ad76839436c0aff4d0fab0561aae21490170b
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-03-16 00:10:13 +0800

    done python5

--- Assignments-Exercises/Python-5_List-and-List-Operations/main.py
+++ Assignments-Exercises/Python-5_List-and-List-Operations/main.py
@@ -1,5 +1,5 @@
 # Step 1: Create a list of integers
-my_list = [5, 3, 8, 1]
+my_list = [5, 3, 8, 1, 9, 2, 7, 4]
 print("Original list:", my_list)
 
 # Step 2: Add an element to the list
@@ -8,6 +8,12 @@ print("List after adding an element:", my_list)
 
 # Step 3: Remove an element from the list
 my_list.remove(8)
+print("List after removing an element:", my_list)
+
+my_list.remove(5)
+print("List after removing an element:", my_list)
+
+my_list.remove(1)
 print("List after removing an element:", my_list)
 
 # Step 4: Sort the list in ascending order
