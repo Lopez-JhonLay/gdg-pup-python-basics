298eececf786ea81fadf44974876ae4599c0ece6
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-03-16 00:21:40 +0800

    done python8

--- Assignments-Exercises/Python-8_OOP-Basics/main.py
+++ Assignments-Exercises/Python-8_OOP-Basics/main.py
@@ -10,8 +10,14 @@ class Car:
     def describe(self):
         return f"This car is a {self.year} {self.make} {self.model}."
 
-# Create an instance of the Car class
+# Create instances of the Car class
 my_car = Car("Toyota", "Corolla", 2020)
+car1 = Car("Honda", "Civic", 2019)
+car2 = Car("Ford", "Mustang", 2022)
+car3 = Car("Tesla", "Model 3", 2021)
 
-# Print the description of the car
+# Print the description of all cars
 print(my_car.describe())
+print(car1.describe())
+print(car2.describe())
+print(car3.describe())
